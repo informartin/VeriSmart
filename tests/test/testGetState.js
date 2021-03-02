5fa41120004b3651c894ee6f901964ad9cacd92e
Leonard Stutzer <leonard.stutzer@campus.tu-berlin.de>
2021-03-02 17:59:22 +0100

    fix test

--- tests/test/testGetState.js
+++ tests/test/testGetState.js
@@ -1,17 +1,19 @@
 const { execSync } = require('child_process');
 const SimpleSmartContractStorage = artifacts.require("SimpleSmartContractStorage");
 const ContractWithLib = artifacts.require("ContractWithLib");
+const ConvertLib = artifacts.require('ConvertLib');
 const Web3 = require('web3');
 const fs = require('fs');
 
 const source_dsl = 'http://localhost:8545';
-const jsonFileName = '../test.json';
+const jsonFileName = 'test.json';
 const validJsonFileName = './test/data/smartContractStorageWithConvertLib.json';
 
 contract('testGetState', (accounts) => {
     it('should return all values/references in state and code to other contracts', async () => {
         const simpleStorageInstance = await SimpleSmartContractStorage.deployed();
         const contractWithLibInstance = await ContractWithLib.deployed();
+        const libInstance = await ConvertLib.deployed();
 
         // saving smart contract lib in state of source contract
         await simpleStorageInstance.setContract.sendTransaction(contractWithLibInstance.address);
@@ -27,11 +29,19 @@ contract('testGetState', (accounts) => {
         let output = execSync(migrateCommand, { cwd: './../' });
         console.log(output.toString());
 
-        const generatedJson = fs.readFileSync(jsonFileName, { encoding: 'utf-8' });
-        const validJson = fs.readFileSync(validJsonFileName, { encoding: 'utf-8' });
+        const generatedJson = JSON.parse(fs.readFileSync(`../${jsonFileName}`, { encoding: 'utf-8' }));
+        const validJson = JSON.parse(fs.readFileSync(validJsonFileName, { encoding: 'utf-8' }));
 
-        execSync(`rm ${jsonFileName}`);
+        // need to dynamically change addresses in the correct state json file
+        validJson["contract_address"] = simpleStorageInstance.address;
+        validJson["state_references"][0] = validJson["state_references"][0].replace(/0x[2]{40}/, contractWithLibInstance.address);
+        validJson["state_references"][0] = validJson["state_references"][0].replace(/73[3]{40}/g, `73${libInstance.address.toLowerCase().substring(2)}`);
+        validJson["state_references"][0] = validJson["state_references"][0].replace(/73[3]{40}/g, `73${libInstance.address.toLowerCase().substring(2)}`);
+        validJson["state_references"][0] = validJson["state_references"][0].replace(/0x12345678910/, libInstance.address);
 
-        expect(generatedJson).to.equal(validJson);
+
+        execSync(`rm ../${jsonFileName}`);
+
+        expect(validJson).to.deep.equal(generatedJson);
     });
 });
