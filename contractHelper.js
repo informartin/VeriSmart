5c3f70449e0c8a3ff5f63e9514b3631331715ca9
Leonard Stutzer <leonard.stutzer@campus.tu-berlin.de>
2021-03-05 21:06:49 +0100

    fix #25: if object too big, use stream api to stream it to file.

--- contractHelper.js
+++ contractHelper.js
@@ -2,6 +2,10 @@ const Web3 = require('web3');
 const { EVM } = require('evm');
 const contractFunc = require('./getContract.js');
 const fs = require("fs");
+const StreamChain = require('stream-chain');
+const Parser = require('stream-json/Parser');
+const BigJson = require('big-json');
+const { stringer } = require('stream-json/Stringer');
 
 const getState = async (contract_address,
                         source_web3,
@@ -18,7 +22,8 @@ const getState = async (contract_address,
     if (!(raw_contract_code.length > 3)) {
         return undefined;
     }
-    const contract = await contractFunc.getContract(contract_address, source_web3, {deployment_tx_hash, csv_path, node, fat_db, targetFile});
+
+    const contract = await contractFunc.getContract(contract_address, source_web3, {deployment_tx_hash, csv_path, node, fat_db, targetFile: undefined});
 
     // Mapping of referenced contract addresses
     let storage = contract.storage;
@@ -38,13 +43,13 @@ const getState = async (contract_address,
                     csv_path,
                     node,
                     fat_db,
-                    targetFile
+                    targetFile: undefined
                 }
             );
             if (referencedContract !== undefined) {
                 console.log('--- Reference found in state: ', value, ' ---');
                 delete storage[index];
-                stateReferencedContracts.push(JSON.stringify(referencedContract, null, 4));
+                stateReferencedContracts.push(referencedContract);
             }
         }
     }
@@ -63,7 +68,7 @@ const getState = async (contract_address,
         }
     }
     // get state of referenced contracts
-    static_references = [];
+    const static_references = [];
     for (const address of staticReferencedContracts) {
         console.log('--- Reference found in static code: ', address, ' ---');
         const reference = await getState(
@@ -74,10 +79,11 @@ const getState = async (contract_address,
                 csv_path,
                 node,
                 fat_db,
-                targetFile
+                targetFile: undefined
             }
         );
-        static_references.push(JSON.stringify(reference, null, 4));
+
+        static_references.push(reference);
     }
 
     const contractState = { 
@@ -88,22 +94,76 @@ const getState = async (contract_address,
         state_references: stateReferencedContracts, 
     };
 
-    if (typeof targetFile !== 'undefined') writeToJson(contractState, targetFile);
+    if (typeof targetFile !== 'undefined') await writeToJson(contractState, targetFile);
 
-    console.log('Complete State of contract:');
-    console.log(JSON.stringify(contractState, null, 4));
+    try {
+        console.log('Complete State of contract:');
+        console.log(JSON.stringify(contractState, null, 4));
+    } catch(e) {
+        if (e instanceof RangeError) {
+            printBigState(contractState);
+        }
+    }
 
     return contractState;
 };
 
-const writeToJson = (storage, targetFile) => {
-    const state = JSON.stringify(storage, null, 4);
-    fs.writeFileSync(targetFile, state, (err) => {
-        if (err) {
-            console.error(err);
-            return;
+const writeToJson = async (storage, targetFile) => {
+    console.log('Writing to file...');
+    try {
+        const state = JSON.stringify(storage, null, 4);
+        fs.writeFileSync(targetFile, state, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+        console.log('Done!');
+    } catch(e) {
+        if (e instanceof RangeError) {
+            console.log('Contract too big for JSON.stringify. Will stream it to the file now.');
+            return new Promise(resolve => {
+                // creating string stream out of storage json
+                const stringifiedStream = BigJson.createStringifyStream({
+                    body: storage
+                });
+
+                // pipelining it to the targetFile
+                const pipeline = StreamChain.chain([
+                    stringifiedStream,
+                    new Parser(),
+                    stringer(),
+                    fs.createWriteStream(targetFile)
+                ]);
+
+                pipeline.on('end', () => {
+                    console.log('Done!');
+                    resolve();
+                })
+            });
+        } else {
+            console.log('Could not write to file.');
         }
-    });
+    }
 };
 
+const printBigState = (storage) => {
+    console.log('Complete State of contract:');
+    console.log(`Contract Address: ${storage.contract_address}`);
+    console.log(`Contract Code: ${storage.raw_contract_code}`);
+    console.log('Contract State (without references):');
+    for (const [key, value] of Object.entries(storage.state)) {
+        console.log(`"${key}": ${value}`);
+    }
+    console.log('Contract State References:');
+    for (const contract of storage.state_references) {
+        printState(contract);
+    }
+    console.log('Contract Static References:');
+    for (const contract of storage.static_references) {
+        printState(contract);
+    }
+}
+
 module.exports.getState = getState;
+module.exports.writeToJson = writeToJson;
