2e6d0d4fb5971075614398d91500a802cec59028
Martin Westerkamp <westerkamp@tu-berlin.de>
2021-03-07 17:25:28 +0100

    limit max recursion

--- contractHelper.js
+++ contractHelper.js
@@ -14,11 +14,13 @@ const getState = async (contract_address,
                             csv_path,
                             node,
                             fat_db,
+                            max_depth,
                             targetFile
                         }) => {
     let raw_contract_code = await source_web3.eth.getCode(contract_address);
     console.log('Code: ', raw_contract_code);
     console.log('Length: ', raw_contract_code.length);
+    console.log('max depth: ', max_depth);
     if (!(raw_contract_code.length > 3)) {
         return undefined;
     }
@@ -29,71 +31,78 @@ const getState = async (contract_address,
     let storage = contract.storage;
     let contract_code = contract.contract_code;
     let stateReferencedContracts = [];
-    //check if storage variable is a contract
-    for (const [index, paddedValue] of Object.entries(storage)) {
-        // remove leading zeros
-        const value = paddedValue.replace(/^0+/, '');
-        if (source_web3.utils.isAddress(value)) {
-            console.log('Address found in Storage: ', value);
-            const checksumAddress = Web3.utils.toChecksumAddress(value);
-            const indexOfExistingReference = stateReferencedContracts.findIndex((referencedContract) => referencedContract.contract_address === checksumAddress);
-            if (indexOfExistingReference < 0) {
-                const referencedContract = await getState(
-                    checksumAddress,
-                    source_web3,
-                    {
-                        deployment_tx_hash,
-                        csv_path,
-                        node,
-                        fat_db,
-                        targetFile: undefined
+    let staticReferencedContracts = [];
+    const static_references = [];
+
+    // only check if recursion limit isn't reached
+    if(max_depth != 0) {
+        //check if storage variable is a contract
+        for (const [index, paddedValue] of Object.entries(storage)) {
+            // remove leading zeros
+            const value = paddedValue.replace(/^0+/, '');
+            if (source_web3.utils.isAddress(value)) {
+                console.log('Address found in Storage: ', value);
+                const checksumAddress = Web3.utils.toChecksumAddress(value);
+                const indexOfExistingReference = stateReferencedContracts.findIndex((referencedContract) => referencedContract.contract_address === checksumAddress);
+                if (indexOfExistingReference < 0) {
+                    const referencedContract = await getState(
+                        checksumAddress,
+                        source_web3,
+                        {
+                            deployment_tx_hash,
+                            csv_path,
+                            node,
+                            fat_db,
+                            max_depth: max_depth - 1,
+                            targetFile: undefined
+                        }
+                    );
+                    if (referencedContract !== undefined) {
+                        console.log('--- Reference found in state: ', value, ' ---');
+                        delete storage[index];
+                        referencedContract['index'] = index;
+                        stateReferencedContracts.push(referencedContract);
                     }
-                );
-                if (referencedContract !== undefined) {
-                    console.log('--- Reference found in state: ', value, ' ---');
-                    delete storage[index];
+                } else {
+                    const referencedContract = Object.assign({}, stateReferencedContracts[indexOfExistingReference]);
                     referencedContract['index'] = index;
+                    delete storage[index];
                     stateReferencedContracts.push(referencedContract);
                 }
-            } else {
-                const referencedContract = Object.assign({}, stateReferencedContracts[indexOfExistingReference]);
-                referencedContract['index'] = index;
-                delete storage[index];
-                stateReferencedContracts.push(referencedContract);
             }
         }
-    }
 
-    // getting all static references
-    const evm = new EVM(contract_code);
-    let staticReferencedContracts = await evm.getOpcodes()
-        .filter( code => (code.name === 'PUSH20') )
-        .map( code => Web3.utils.toChecksumAddress(`0x${code.pushData.toString('hex')}`) )
-        .filter( address => (address.search(/0x[fF]{40}/) === -1 && address !== contract_address ) );
-    // filter out all EOAs
-    for (const address of staticReferencedContracts) {
-        const contractCode = await source_web3.eth.getCode(address);
-        if (!(contractCode.length > 3)) {
-            staticReferencedContracts.splice(staticReferencedContracts.indexOf(address), 1);
-        }
-    }
-    // get state of referenced contracts
-    const static_references = [];
-    for (const address of staticReferencedContracts) {
-        console.log('--- Reference found in static code: ', address, ' ---');
-        const reference = await getState(
-            Web3.utils.toChecksumAddress(address),
-            source_web3,
-            {
-                deployment_tx_hash,
-                csv_path,
-                node,
-                fat_db,
-                targetFile: undefined
+        // getting all static references
+        const evm = new EVM(contract_code);
+        staticReferencedContracts = await evm.getOpcodes()
+            .filter( code => (code.name === 'PUSH20') )
+            .map( code => Web3.utils.toChecksumAddress(`0x${code.pushData.toString('hex')}`) )
+            .filter( address => (address.search(/0x[fF]{40}/) === -1 && address !== contract_address ) );
+        // filter out all EOAs
+        for (const address of staticReferencedContracts) {
+            const contractCode = await source_web3.eth.getCode(address);
+            if (!(contractCode.length > 3)) {
+                staticReferencedContracts.splice(staticReferencedContracts.indexOf(address), 1);
             }
-        );
+        }
+        // get state of referenced contracts
+        for (const address of staticReferencedContracts) {
+            console.log('--- Reference found in static code: ', address, ' ---');
+            const reference = await getState(
+                Web3.utils.toChecksumAddress(address),
+                source_web3,
+                {
+                    deployment_tx_hash,
+                    csv_path,
+                    node,
+                    fat_db,
+                    max_depth: max_depth - 1,
+                    targetFile: undefined
+                }
+            );
 
-        static_references.push(reference);
+            static_references.push(reference);
+        }
     }
 
     const contractState = { 
